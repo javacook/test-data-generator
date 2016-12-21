f282d7948a0613e6ecfa509ce11797e64698141f
javacook <javacook@gmx.de>
2016-12-21 09:53:54 +0100

    initial

---
+++ src/test/java/com/javacook/testdatagenerator/TestDataGeneratorMain.java
@@ -0,0 +1,21 @@
+package com.javacook.testdatagenerator;
+
+import com.javacook.easyexcelaccess.ExcelCoordinate;
+import com.javacook.testdatagenerator.TestDataGenerator.ExcelCell;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vollmer on 21.12.16.
+ */
+public class TestDataGeneratorMain {
+
+    public static void main(String[] args) throws IOException {
+        TestDataGenerator testDataGenerator = new TestDataGenerator("TestdataForTest.xls", 2, 0);
+        final ExcelCell cell = testDataGenerator.readCell(0, new ExcelCoordinate("C", 2));
+        System.out.println(cell);
+    }
+
+}
